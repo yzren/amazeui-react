64406aeb7bfbed6cddc6d8f87f9792374c35a862
minwe <minwe@yunshipei.com>
2015-12-22 12:28:11 +0800

    add escape method and highlight prop to Code

--- src/Code.js
+++ src/Code.js
@@ -8,23 +8,43 @@ var Code = React.createClass({
   mixins: [ClassNameMixin],
 
   propTypes: {
+    language: React.PropTypes.string,
+    escape: React.PropTypes.bool,
+    highlight: React.PropTypes.func
   },
 
   getDefaultProps: function() {
   },
 
+  escape: function(html) {
+    return html.replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/"/g, '&quot;')
+      .replace(/'/g, '&#39;');
+  },
+
   render: function() {
+    var langClassName = this.props.language &&
+      ('language-' + this.props.language);
+    var children = this.props.children;
+
+    if (this.props.escape) {
+      children = this.escape(children);
+    }
+
+    if (this.props.highlight) {
+      children = this.props.highlight(language, children);
+    }
+
     return (
-      <pre>
-        {this.props.children}
+      <pre
+        {...this.props}
+        className={classNames(this.props.className, langClassName)}
+      >
+        {children}
       </pre>
     );
   }
 });
 
 module.exports = Code;
-
-// TODO:
-// - 自动编码
-// - highlight
-// 创建一个独立插件应该更好一些
